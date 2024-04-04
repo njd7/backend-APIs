8c6aefbc820d635ca0ac469f7a62205b7f41c699
nayan <nayannjddas@gmail.com>
2024-04-04 20:06:10 +0530

    ..

--- src/routes/misc.routes.js
+++ src/routes/misc.routes.js
@@ -12,7 +12,7 @@ const router = Router();
  *   name: Authentication
  *   description: Endpoints related to user authentication and authorization
  *
- * /secured-page:
+ * /misc/secured-page:
  *   get:
  *     summary: Get secured content
  *     tags: [Authentication]
